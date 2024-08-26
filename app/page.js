545f29fb2853a808356d855db4e0809ad35285ba
karim <fifakarim52@gmail.com>
2024-08-26 11:20:36 +0100

    test opn graph

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
 import Landing from "@/components/component/landing";
 
+
+
+export const metadata = {
+    title:'Welcome to Aosta Hub',
+    description: 'Discover Aosta Hub, a unique hub where people come together to share their stories',
+    icons:{
+        icon:'/ass/logo.png'
+    },
+    openGraph:{
+        title:'Welcome to Aosta Hub',
+    }
+
+}
 export default function Home() {
-    return ( <
-        Landing / >
+    return (
+        <>
+            <Landing />
+        </>
     );
 }
