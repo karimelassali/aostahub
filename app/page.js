e54f66ee5a12bf09d2262aa322b58177c54976cb
karim <fifakarim52@gmail.com>
2024-08-26 12:15:25 +0100

    test opn graph 6

--- app/page.js
+++ app/page.js
@@ -3,25 +3,25 @@ import Landing from "@/components/component/landing";
 
 
 export const metadata = {
-    title:'Welcome to Aosta Hub',
+    title: 'Welcome to Aosta Hub',
     description: 'Discover Aosta Hub, a unique hub where people come together to share their stories',
-    
     keywords: 'Aosta Hub, Story Sharing, Community, Valle di Aosta, amici, storytelling, social platform, local community', 
-    icons:{
-        icon:'/ass/logo.png'
+    icons: {
+        icon: '/ass/logo.png'
     },
-    openGraph:{
-        title:'Welcome to Aosta Hub',
-        description:'Discover Aosta Hub, a unique hub where people come together to share their stories',
-        url:'https://aostahub.vercel.app',
-        images:{
-            url:'/ass/logo.png',
-            width:800,
-            height:600,
+    openGraph: {
+        title: 'Welcome to Aosta Hub',
+        description: 'Discover Aosta Hub, a unique hub where people come together to share their stories',
+        url: 'https://aostahub.vercel.app',
+        images: {
+            url: '/ass/logo.png',
+            width: 800,
+            height: 600,
         }
-    }
-
+    },
+    googleSiteVerification: 'TFC9KKJST9sPJHv4r0wz0xnmUb09ZFJFC8crGzgUSnk'
 }
+
 export default function Home() {
     return (
         <>
