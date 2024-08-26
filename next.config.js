88a3cdf7f112300cf6d5d8fa99b0145110c946d6
karim <fifakarim52@gmail.com>
2024-08-26 11:52:36 +0100

    test opn graph 3

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 // next.config.js
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['img.clerk.com'], // Add any additional domains as needed
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  images: {
+    domains: ['img.clerk.com'], // Add any additional domains as needed
+  },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'google-site-verification',
+            value: 'wGNA7UCO_gV1Gpyd2ccb4kg_OvPu-1V',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
