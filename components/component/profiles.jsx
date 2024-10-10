c37893ebb5243692e4b29c87c055f3b5de6b97cb
karim <fifakarim52@gmail.com>
2024-10-10 14:23:40 +0100

    add stars in confiitiit

--- components/component/profiles.jsx
+++ components/component/profiles.jsx
@@ -9,7 +9,7 @@ import { useUser } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
 import { useRouter } from 'next/navigation'
-import { ConfettiButton } from '../ui/confetti'
+import confetti from "canvas-confetti";
 
 // const profiles = [
 //   {
@@ -44,6 +44,37 @@ import { ConfettiButton } from '../ui/confetti'
 //   },
 // ]
 
+  const handleopenConfetti = () => {
+    const defaults = {
+      spread: 360,
+      ticks: 50,
+      gravity: 0,
+      decay: 0.94,
+      startVelocity: 30,
+      colors: ["#FFE400", "#FFBD00", "#E89400", "#FFCA6C", "#FDFFB8"],
+    };
+ 
+    const shoot = () => {
+      confetti({
+        ...defaults,
+        particleCount: 40,
+        scalar: 1.2,
+        shapes: ["star"],
+      });
+ 
+      confetti({
+        ...defaults,
+        particleCount: 10,
+        scalar: 0.75,
+        shapes: ["circle"],
+      });
+    };
+ 
+    setTimeout(shoot, 0);
+    setTimeout(shoot, 100);
+    setTimeout(shoot, 200);
+  };
+ 
 const interestIcons = {
   Coffee: <Coffee />,
   Travel: <Plane />,
@@ -112,10 +143,7 @@ const Profiles = () => {
         liker: liker,
         receiver: receiver
       })  
-        const confetti = document.getElementById('confetti');
-        if (confetti) {
-            confetti.click();
-        }
+      handleopenConfetti();
       const aud = new Audio('/ass/like.wav')
       if (error) {
         toast.error('Something went wrong.')
