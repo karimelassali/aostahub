6843d7da9bd282b5fbe037c91f660ae9c92d71a6
karim <fifakarim52@gmail.com>
2024-11-05 15:18:13 +0100

    fix start checking notifications1

--- components/component/landing.jsx
+++ components/component/landing.jsx
@@ -177,27 +177,21 @@ export default function Landing() {
       localStorage.setItem("cookieStatu", "seen");
       setCookieStatu('notYet');
     }
-    function notifyMe() {
-      if (!("Notification" in window)) {
-        // Check if the browser supports notifications
-        alert("This browser does not support desktop notification");
-      } else if (Notification.permission === "granted") {
-        // Check whether notification permissions have already been granted;
-        // if so, create a notification
-        const notification = new Notification("Hi there!");
-        // …
-      } else if (Notification.permission !== "denied") {
-        // We need to ask the user for permission
-        Notification.requestPermission().then((permission) => {
-          // If the user accepts, let's create a notification
-          if (permission === "granted") {
-            const notification = new Notification("Hi there!");
-            // …
-          }
-        });
-      }
-    }
-    notifyMe();
+   // Request permission from the user to send notifications
+   Notification.requestPermission().then(permission => {
+     if (permission === 'granted') {
+       // Create a new notification
+       const notification = new Notification('Hello, world!', {
+         body: 'This is a notification',
+         icon: 'https://example.com/icon.png',
+       });
+   
+       // Add an event listener to the notification
+       notification.addEventListener('click', () => {
+         console.log('Notification clicked!');
+       });
+     }
+   });
 
   },[])
 
