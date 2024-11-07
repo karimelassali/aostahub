a96f4f59603c7c20ebcc23082a75e73a682196f3
karim <fifakarim52@gmail.com>
2024-11-07 17:50:33 +0100

    add file input to rosta v0.2 -rosta endpoint

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -4,23 +4,23 @@ import { auth, currentUser } from '@clerk/nextjs/server'
 
 const resend = new Resend('re_KNzDDTwx_8gKv5LreysS8qmpvXThaSmkn');
 
+/**
+ * POST /api/email
+ *
+ * Send a welcome email to the user using resend.
+ *
+ * @returns {Promise<Response>}
+ */
 export async function POST() {
-  try {
-    const current = await currentUser()
-    if(current.fullName){
-      const { data, error } = await resend.emails.send({
-        from: 'AOSTAHUB <onboarding@resend.dev>',
-        to: [`${current.emailAddresses[0].emailAddress}`],
-        subject: 'Welcome into AOSTAHUB',
-        react: KoalaWelcomeEmail({userFirstname:`${current.fullName}`}),
-      });
-      if (error) {
-        return Response.json({ error }, { status: 500 });
-      }
-      return Response.json(current);
-    }
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-    
+  const current = await currentUser();
+  if(current.fullName){
+    await resend.emails.send({
+      from: 'AOSTAHUB <onboarding@resend.dev>',
+      to: [current.emailAddresses[0].emailAddress],
+      subject: 'Welcome into AOSTAHUB',
+      react: KoalaWelcomeEmail({userFirstname: current.fullName}),
+    });
+    return Response.json(current);
   }
 }
+
