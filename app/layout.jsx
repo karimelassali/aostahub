ba49f3eda57f12ec976b0c0512ac236575e089d2
karim <fifakarim52@gmail.com>
2024-11-25 18:40:44 +0100

    Add reply to feature

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,3 @@
-'use client'
 import {
   ClerkProvider,
 } from "@clerk/nextjs";
@@ -7,6 +6,8 @@ import "@radix-ui/themes/styles.css";
 import { ThemeProvider } from "@/components/ui/theme-provider"
 
 import { Neobrutalism } from '@clerk/themes'
+import { Analytics } from "@vercel/analytics/react"
+
 
 
 
@@ -14,47 +15,47 @@ import { Neobrutalism } from '@clerk/themes'
 
 
 
-// export const metadata = {
-//   title: "Welcome to Aosta Hub",
-//   description:
-//     "Discover Aosta Hub, a vibrant community platform where locals and visitors share their stories and experiences about the beautiful Valle di Aosta region.",
-//   keywords:
-//     "Aosta Hub, Story Sharing, Community, Valle di Aosta, local stories, storytelling, social platform, local community, travel, culture, adventure",
-//   icons: {
-//     icon: "/ass/logo.png",
-//   },
-//   openGraph: {
-//     title: "Welcome to Aosta Hub",
-//     description:
-//       "Discover Aosta Hub, a vibrant community platform where locals and visitors share their stories and experiences about the beautiful Valle di Aosta region.",
-//     url: "https://aostahub.vercel.app",
-//     images: {
-//       url: "/ass/logo.png",
-//       width: 800,
-//       height: 600,
-//     },
-//     site_name: "Aosta Hub",
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     title: "Welcome to Aosta Hub",
-//     description:
-//       "Discover Aosta Hub, a vibrant community platform where locals and visitors share their stories and experiences about the beautiful Valle di Aosta region.",
-//     images: {
-//       url: "/ass/logo.png",
-//       width: 800,
-//       height: 600,
-//     },
-//     site: "@aostahub",
-//   },
-//   robots: {
-//     index: true,
-//     follow: true,
-//     nocache: true,
-//     googlebot: "index,follow",
-//   },
-//   googleSiteVerification: "TFC9KKJST9sPJHv4r0wz0xnmUb09ZFJFC8crGzgUSnk",
-// };
+export const metadata = {
+  title: "Welcome to Aosta Hub",
+  description:
+    "Discover Aosta Hub, a vibrant community platform where locals and visitors share their stories and experiences about the beautiful Valle di Aosta region.",
+  keywords:
+    "Aosta Hub, Story Sharing, Community, Valle di Aosta, local stories, storytelling, social platform, local community, travel, culture, adventure",
+  icons: {
+    icon: "/ass/logo.png",
+  },
+  openGraph: {
+    title: "Welcome to Aosta Hub",
+    description:
+      "Discover Aosta Hub, a vibrant community platform where locals and visitors share their stories and experiences about the beautiful Valle di Aosta region.",
+    url: "https://aostahub.vercel.app",
+    images: {
+      url: "/ass/logo.png",
+      width: 800,
+      height: 600,
+    },
+    site_name: "Aosta Hub",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Welcome to Aosta Hub",
+    description:
+      "Discover Aosta Hub, a vibrant community platform where locals and visitors share their stories and experiences about the beautiful Valle di Aosta region.",
+    images: {
+      url: "/ass/logo.png",
+      width: 800,
+      height: 600,
+    },
+    site: "@aostahub",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googlebot: "index,follow",
+  },
+  googleSiteVerification: "TFC9KKJST9sPJHv4r0wz0xnmUb09ZFJFC8crGzgUSnk",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -73,6 +74,7 @@ export default function RootLayout({ children }) {
           />
         </head>
         <body className="">
+        <Analytics/>
 
         <ThemeProvider
             attribute="class"
